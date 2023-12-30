93af8618f73cd4e128ff2dd061caead2820db198
Jim Kutter <rwg1138@gmail.com>
2023-12-30 18:51:53 -0500

    initial commit

---
+++ TimeStopHello.cs
@@ -0,0 +1,10 @@
+using Terraria.GameContent.UI;
+using Terraria.ModLoader;
+
+namespace TimeStopHello
+{
+	// This is a partial class, meaning some of its parts were split into other files. See ExampleMod.*.cs for other portions.
+	public partial class TimeStopHello : Mod
+	{
+	}
+}
